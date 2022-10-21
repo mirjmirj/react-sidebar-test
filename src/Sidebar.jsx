a457244e246d6f00db8302332eca52084df27a77
mirj <mirj.developer@gmail.com>
2022-10-21 15:10:00 -0500

    Initial UI

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -2,8 +2,88 @@ import { useState } from 'react';
 import * as Icons from './assets/Icons';
 import './Sidebar.scss';
 
-export default function Sidebar() {
+function Sidebar() {
+  const [isShrinkView, setIsShrinkView] = useState(false);
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const handleSidebarView = () => {
+    setIsShrinkView(!isShrinkView);
+  };
+
+  const handleThemeChange = () => {
+    setIsDarkMode(!isDarkMode);
+    document.body.classList.toggle("dark");
+  };
+
   return (
-    <h1>Hello World</h1>
+    <div className={`sidebar-container${isShrinkView ? " shrink" : ""}`}>
+      <button
+        className="sidebar-viewButton"
+        type="button"
+        aria-label={isShrinkView ? "Expand Sidebar" : "Shrink Sidebar"}
+        title={isShrinkView ? "Expand" : "Shrink"}
+        onClick={handleSidebarView}
+      >
+      </button>
+      <div className="sidebar-wrapper">
+        <div className="sidebar-themeContainer">
+          <label
+            htmlFor="theme-toggle"
+            className={`sidebar-themeLabel${isDarkMode ? " switched" : ""}`}
+          >
+            <input
+              className="sidebar-themeInput"
+              type="checkbox"
+              id="theme-toggle"
+              onChange={handleThemeChange}
+            />
+            <div className="sidebar-themeType light">
+              <span className="sidebar-themeInputText">Light</span>
+            </div>
+            <div className="sidebar-themeType dark">
+              <span className="sidebar-themeInputText">Dark</span>
+            </div>
+          </label>
+        </div>
+        <ul className="sidebar-list">
+          <li className="sidebar-listItem active">
+            <a>
+              <span className="sidebar-listItemText">Dashboard</span>
+            </a>
+          </li>
+          <li className="sidebar-listItem">
+            <a>
+              <span className="sidebar-listItemText">Inbox</span>
+            </a>
+          </li>
+          <li className="sidebar-listItem">
+            <a>
+              <span className="sidebar-listItemText">Calendar</span>
+            </a>
+          </li>
+          <li className="sidebar-listItem">
+            <a>
+              <span className="sidebar-listItemText">Activity</span>
+            </a>
+          </li>
+          <li className="sidebar-listItem">
+            <a>
+              <span className="sidebar-listItemText">Settings</span>
+            </a>
+          </li>
+        </ul>
+        <div className="sidebar-profileSection">
+          <img
+            src=""
+            width="40"
+            height="40"
+            alt="Mitch Thompson"
+          />
+          <span>Mitch Thompson</span>
+        </div>
+      </div>
+    </div>
   );
-}
+};
+
+export default Sidebar;
